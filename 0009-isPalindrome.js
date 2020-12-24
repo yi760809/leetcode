32e45e0ec28ce04047513d4377e2b03a050c2f8c
json_yu <dizu7608@126.com>
2020-12-24 22:13:12 +0800

    回文数

---
+++ 0009-isPalindrome.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if(x < 0) return false
+  x = x.toString()
+  var str1 = x
+  var str2 = ''
+  for(var i = x.length; i >= 0; i --) {
+    str2 += x.charAt(i)
+  }
+  if(str1 === str2) {
+    return true
+  }else {
+    return false
+  }
+};
+
+console.log(isPalindrome(-1234321))
