9226d8a3fd5788254a6d7fd6949ef9d606f00814
yipengcheng <yipengcheng@freedotech.com>
2021-01-25 10:44:23 +0800

    分发糖果

---
+++ 0135-candy.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+var candy = function(ratings) {
+  var arr = new Array(ratings.length).fill(1)
+  for(var i = 0; i < ratings.length; i ++) {
+    if(ratings[i] > ratings[i-1]) {
+      arr[i] = arr[i-1] + 1
+    }
+  }
+  for(var i = ratings.length - 1; i >=0; i --) {
+    if(ratings[i] < ratings[i-1]) {
+      arr[i-1] = Math.max(arr[i] + 1, arr[i - 1]);
+    }
+  }
+  var sum = 0
+  arr.forEach(item => {
+    sum += item
+  })
+  return sum
+};
+var t = [1,2,2,3,1,4]
+console.log(candy(t))
